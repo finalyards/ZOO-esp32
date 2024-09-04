c443d0122c366c229d25f08fa56b93aaad411a3c
Asko Kauppi <demo@outstanding.earth>
2024-09-04 17:20:09 +0300

    latest for #4

--- vl53l5cx_uld/examples/pins.rs
+++ vl53l5cx_uld/examples/pins.rs
@@ -1,16 +1,29 @@
 
 use esp_hal::{
     gpio::{Io, InputPin, OutputPin, NO_PIN},
+    peripheral::Peripheral
 };
 
+/***R
+* This does NOT compile:
 #[allow(non_camel_case_types)]
-pub fn get_pins<SDA,SCL,PWR_EN,_RESERVED>(&io: &Io) -> (SDA,SCL,Option<PWR_EN>,Option<_RESERVED>)
-    where SDA: OutputPin + InputPin,
-        SCL: OutputPin + InputPin,
-        PWR_EN: OutputPin,
-        _RESERVED: OutputPin        // tbd. either I2C_RST and/or INT (pull down pin)
-{
-    // changed via running './set-target.sh'
+pub fn get_pins<SDA,SCL,PWR_EN,_RESERVED>(&io: &Io) -> (SDA, SCL, Option<PWR_EN>,Option<_RESERVED>)
+    where SDA: Peripheral<P = impl OutputPin + InputPin>,
+        SCL: Peripheral<P = impl OutputPin + InputPin>,
+        PWR_EN: Peripheral<P = impl OutputPin>,
+        _RESERVED: Peripheral<P = impl OutputPin>        // tbd. either I2C_RST and/or INT (pull down pin)
+    { }
+***/
+
+// Q: What's wrong with the code below?
+//
+#[allow(non_camel_case_types)]
+pub fn get_pins<SDA,SCL,PWR_EN,_RESERVED>(&io: &Io) -> (SDA, SCL, Option<PWR_EN>,Option<_RESERVED>)
+where SDA: Peripheral<P: OutputPin + InputPin>,
+      SCL: Peripheral<P: OutputPin + InputPin>,
+      PWR_EN: Peripheral<P: OutputPin>,
+      _RESERVED: Peripheral<P: OutputPin>,       // tbd. either I2C_RST and/or INT (pull down pin)
+{   // changed via running './set-target.sh'
     (io.pins.gpio4, io.pins.gpio5, Some(io.pins.gpio0), NO_PIN)      // esp32c3
     //(io.pins.gpio22, io.pins.gpio23, Some(io.pins.gpio21), NO_PIN)    // esp32c6
 }
