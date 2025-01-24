7a35a24e7aa3ecf722fd64d3151608fb0b172ef0
demo <demo@outstanding.earth>
2025-01-24 11:31:12 +0200

    [comms] cleanup

--- comms/ble-custom/src/gatt_server.rs
+++
@@ -1,8 +0,0 @@
-/*
-* Wrap around any '#[gatt_server]' struct
-*/
-trait XGattServer {
-    fn new_with_name(name: &str) -> Result<Self,Error>;
-
-    fn ctic_by_handle(&mut self, handle: u16) -> Option<&mut MagicService>;
-}
